578a60f63e8b79d6bf957d774c44680bd01ba78d
gonglei <gonglei@encdata.cn>
2020-05-07 17:44:43 +0800

    修改输出

--- src/effectivejava/chapter2/item2/builder/NutritionFacts.java
+++ src/effectivejava/chapter2/item2/builder/NutritionFacts.java
@@ -49,8 +49,8 @@ public class NutritionFacts {
     }
 
     public static void main(String[] args) {
-        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
-                .calories(100).sodium(35).carbohydrate(27).build();
+        NutritionFacts cocaCola = new Builder(240, 8)
+                .calories(100).fat(1000).sodium(35).carbohydrate(27).build();
         System.out.println(cocaCola.calories);
     }
 }
